5e33648b7e71695eaf1230cc6b61dec8f4947473
Aquino_U2L <lmaquino2@up.edu.ph>
2025-04-26 17:55:53 +0800

    style: implemented the face square in the cam part

--- frontend/pages/face-scanning.vue
+++ frontend/pages/face-scanning.vue
@@ -35,7 +35,9 @@
             @canplay="onVideoCanPlay"
             @play="onVideoPlay"
           ></video>
-          <div class="face-square"></div>
+          <div class="face-square">
+            <span></span>
+          </div>
         </div>
         <div class="face-subtitle">Put your face here</div>
       </div>
